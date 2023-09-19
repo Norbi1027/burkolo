e41a1ce28b9ae12937bda12b4c3b3c8ecbe142cb
Norbi1027 <sandornorbi7@gmail.com>
2023-09-19 08:39:10 +0200

    Add project files.

---
+++ burkolo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace burkolo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adja meg a szélességet.");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Adja meg a hosszusága.");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Adja meg a magasságát.");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            double terulet = 2 * (a + b) * c + a * b;
+            double terulet10 = terulet * 1.10;
+
+            double csempe = terulet10 / 1.44;
+            Console.WriteLine($"\n A terület:{terulet} \n Ennyi csempe kell:{Math.Ceiling(csempe)} ");
+
+            Console.ReadKey();
+        }
+    }
+}
